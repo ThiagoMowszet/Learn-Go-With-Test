98fc16039be8864a94bea2b05b750e98191d4cd3
Thiago <thiagomowszet@gmail.com>
2023-11-30 11:39:46 -0300

    finishing lesson: struct, methods and interfaces

--- strucs_methods_and_interface/shapes.go
+++ strucs_methods_and_interface/shapes.go
@@ -1,29 +1,49 @@
 package main
 
+import "math"
+
 type Rectangle struct {
-	width  float64
-	height float64
+	Width  float64
+	Height float64
 }
 
-// Syntax to declare methods
 // A method is a function with a receiver. A method declaration binds an identifier, the method name, to a method, and associates the method with the receiver's base type.
+
+// Methods are very similar to functions but they are called by invoking them on an instance of a particular type.
+// Where you can just call functions wherever you like, such as Area(rectangle) you can only call methods on "things".
+
+// Syntax to declare methods
 // NOTE: func (receiverName ReceiverType) MethodName(args).
 func (r Rectangle) Area() float64 {
-	return 0
+	return r.Width * r.Height
 }
 
 type Circle struct {
 	Radius float64
 }
 
+// NOTE: It is a convention in Go to have the receiver variable be the first letter of the type -> c:Circle
 func (c Circle) Area() float64 {
-	return 0
+	return math.Pi * c.Radius * c.Radius
 }
 
 func Permiter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.height + rectangle.width)
+	return 2 * (rectangle.Height + rectangle.Width)
 }
 
 func Area(rectangle Rectangle) float64 {
-	return rectangle.height * rectangle.width
+	return rectangle.Height * rectangle.Width
+}
+
+type Shape interface {
+	Area() float64
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
 }
--- strucs_methods_and_interface/shapes_test.go
+++ strucs_methods_and_interface/shapes_test.go
@@ -4,11 +4,11 @@ import "testing"
 
 func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
-	got := Permiter(rectangle) 
+	got := Permiter(rectangle)
 	want := 40.0
 
 	if got != want {
-		// NOTE: The f is for our float64 and the .2 means print 2 decimal places. 
+		// NOTE: The f is for our float64 and the .2 means print 2 decimal places.
 		t.Errorf("\nGOT = %2.f\nWANT = %.2f\n", got, want)
 	}
 }
@@ -19,32 +19,115 @@ func TestArea(t *testing.T) {
 	want := 72.0
 
 	if got != want {
-		// NOTE: The f is for our float64 and the .2 means print 2 decimal places. 
+		// NOTE: The f is for our float64 and the .2 means print 2 decimal places.
 		t.Errorf("\nGOT = %2.f\nWANT = %.2f\n", got, want)
 	}
 }
 
-
 func TestArea2(t *testing.T) {
-	t.Run("rectangles", func(t *testing.T){
-			rectangle := Rectangle{12.0, 6.0}
-			got := rectangle.Area()
-			want := 72.0
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		got := rectangle.Area()
+		want := 72.0
 
-			if got != want {
+		if got != want {
 			// NOTE: Use of g will print a more precise decimal number in the error message
 			t.Errorf("\nGOT = %g\nWANT = %g\n", got, want)
 		}
 	})
 
-	t.Run("rectangles", func(t *testing.T){
-			circle := Circle{10}
-			got := circle.Area()
-			want := 314.1592653589793 
+	t.Run("rectangles", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
 
-			if got != want {
+		if got != want {
 			t.Errorf("\nGOT = %g\nWANT = %g\n", got, want)
 		}
 	})
 
+}
+
+func TestArea3(t *testing.T) {
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+
+		if got != want {
+			t.Errorf("\nGOT = %g\nWANT = %g\n", got, want)
+		}
+	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		got := rectangle.Area()
+		want := 72.0
+
+		if got != want {
+			checkArea(t, rectangle, 72.0)
+		}
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			checkArea(t, circle, 314.1592653589793)
+		}
+	})
+}
+
+// Table Driven Tests
+func TestArea4(t *testing.T) {
+	// anonymous struct
+	areaTest := []struct {
+		shape Shape
+		want  float64
+	}{
+
+		/*{Rectangle{12, 6}, 72.0},
+		  {Circle{10}, 314.1592653589793},
+		  {Triangle{12, 6}, 36.0},*/
+
+        // NOTE: So far you've only been shown syntax for creating instances of structs MyStruct{val1, val2} but you can optionally name the fields.
+		{shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
+		{shape: Circle{Radius: 10}, want: 314.1592653589793},
+		{shape: Triangle{Base: 12, Height: 6}, want: 36.0}}
+
+	for _, tt := range areaTest {
+		got := tt.shape.Area()
+
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
+		}
+
+	}
+}
+
+
+func TestArea5(t *testing.T) {
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		// using tt.name from the case to use it as the `t.Run` test name
+        // And you can run specific tests within your table with `go test -run TestArea/Rectangle`.
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+                // NOTE: The %#v format string will print out our struct with the values in its field, so the developer can see at a glance the properties that are being tested.
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
+
+	}
 }
